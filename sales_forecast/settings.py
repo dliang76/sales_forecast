4864498ec294efbbb29dfbccd1c9d3a5cbf504cd
Dennis Liang <Dennis.Liang@Hilton.com>
2024-11-19 05:12:15 -0500

    reverted debug=True; setting DEBUG=False breaks the API

--- sales_forecast/settings.py
+++ sales_forecast/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-yj1%p7^qu85(gbqd_)*p2=#g3+bvb3o+1-f8k-pm_upy#p-=p_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
